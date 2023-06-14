8f4c2669dfa3f316d9e7bd55871081be84eb5bba
Rosaka12 <92648981+Rosaka12@users.noreply.github.com>
2023-06-14 21:12:57 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementsByClassName("fa-brands fa-youtube")[0].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[1].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("fa-brands fa-youtube")[2].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[0].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[1].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[2].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[3].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[4].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
+
+document.getElementsByClassName("article")[5].addEventListener('click', 
+function() {
+    window.open("https://euphonious-meringue-1da142.netlify.app/");
+})
